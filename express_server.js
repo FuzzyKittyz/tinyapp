ae9521fea953744f2bf5bdc1ad5c1d486b62eeb3
FuzzyKittyz <topher.a@hotmail.com>
2022-09-14 13:11:40 -0600

    Add redirect and link the short url to long url in database

--- express_server.js
+++ express_server.js
@@ -6,15 +6,36 @@ app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
 
+function generateRandomString() {
+  let result           = '';
+  let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let charactersLength = characters.length;
+  for ( var i = 0; i <= 6; i++ ) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+// req -- params, body, query, headers
+// /url/:id. /urls/1  -----params
+// body is any form data
+// query is url query params. Ex /urls/1?text=now
 app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  const key = generateRandomString();
+  urlDatabase[key] = req.body.longURL;
+  //res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  res.redirect(`/urls/${key}`);
 });
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id] 
+  res.redirect(longURL);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -42,8 +63,8 @@ app.get("/urls/new", (req, res) => {
 });
 
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: "http://www.lighthouselabs.ca" };
+  const { id } = req.params; // object destructuring  -->> const id = req.params.id
+  const templateVars = { id, longURL: urlDatabase[id] };
   res.render("urls_show", templateVars);
 });
 
-function generateRandomString() {}
