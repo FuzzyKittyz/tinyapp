205c82a186d4bf0887ace51e3fa845d00c08ef61
FuzzyKittyz <topher.a@hotmail.com>
2022-09-22 10:33:19 -0600

    add more comments

--- helpers.js
+++ helpers.js
@@ -1,11 +1,12 @@
-function checkValue(string, key, users) {
+function checkValue(string, key, users) { //string is what you are getting it to check, key is the key in the obj, users is what database you use it in (users or urldatabase)
   for (let user in users) {
     let value = users[user];
     if (string === value[key]){
-      return users[user]
+      return users[user];//if string is eq return that specific bj
     };
   };
-  return null
+  return null;
 };
+
 
 module.exports = checkValue;
